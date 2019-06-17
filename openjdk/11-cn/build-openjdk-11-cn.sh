9db727110abdd046a398c6cb3eb6f393212f83de
bobo <bobolau@live.com>
2019-06-18 01:08:13 +0800

    add docker image szopen/openjdk:11-jdk-cn

---
+++ openjdk/11-cn/build-openjdk-11-cn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+
+artifactId=openjdk
+version=11-jdk-cn
+
+# docker
+docker build -t ${artifactId}:${version} -f ./Dockerfile .
+#docker tag  ${artifactId}:${version}  ${artifactId}:latest
+
+# hub.docker.com/szopen
+group=szopen
+#docker login -u szopen -p
+docker tag ${artifactId}:${version} ${group}/${artifactId}:${version}
+#docker push ${group}/${artifactId}:${version}
